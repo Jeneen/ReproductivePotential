336e114b1566eeb81b245f4fdf4247278dc40d46
Jeneen <31858778+Jeneen@users.noreply.github.com>
2024-05-01 09:04:29 +0100

    update install.R

--- scripts/install.R
+++ scripts/install.R
@@ -1,4 +1,3 @@
-
 install.packages(c("readr", "plyr", "tidyverse", "rfishbase", "fishtree", "ape", "Hmisc", "brms", 
                      "dplyr",  "fields", "ncdf4", "tidybayes", "picante", "parallel", 
                      "broom", "sp", "purrr", "viridis", "RColorBrewer", "ggrepel", 
@@ -7,4 +6,4 @@ install.packages(c("readr", "plyr", "tidyverse", "rfishbase", "fishtree", "ape",
                      "sjPlot", "ggplotify", "openxlsx"))
 install.packages("https://cran.r-project.org/src/contrib/Archive/rgeos/rgeos_0.6-4.tar.gz", 
                  repos=NULL, type="source")
-devtools::install_github("nschiett/fishflux", dependencies=TRUE)
+
